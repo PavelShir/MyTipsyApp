9ef7dc93aab753a4c24c7d79018f2ee20d46027a
Pavel Shirokiy <pshirokiy@gmail.com>
2024-03-05 18:38:55 +0300

    Initial Commit

---
+++ MyTipsyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTipsyApp
+//
+//  Created by Павел Широкий on 05.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
